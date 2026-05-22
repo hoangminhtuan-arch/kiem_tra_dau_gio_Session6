7b5ef21a465890865f3abda244cfd0b520545ad8
hoangminhtuan-arch <tuan2382007t@gmail.com>
2026-05-22 10:01:06 +0700

    Update bai_kiem_tra_dau_gio.py

--- bai_kiem_tra_dau_gio.py
+++ bai_kiem_tra_dau_gio.py
@@ -1,30 +1,30 @@
 # # CAU 1
-# price = float(input("Moi ban nhap vao don gia: "))
-# quantity = int(input("Moi bannhap vao so luong: "))
+price = float(input("Moi ban nhap vao don gia: "))
+quantity = int(input("Moi bannhap vao so luong: "))
 
-# total_money = price * quantity
+total_money = price * quantity
 
-# if(total_money >= 1000000):
-#     total_money = total_money - (total_money * 0.1)
-#     print("Ban duoc giam gia 10%")
-#     print(f"So tien sau khi duoc giam la {total_money} nghin dong")
-# else:
-#     print("Ban khong duoc giam gia!!!")
+if(total_money >= 1000000):
+    total_money = total_money - (total_money * 0.1)
+    print("Ban duoc giam gia 10%")
+    print(f"So tien sau khi duoc giam la {total_money} nghin dong")
+else:
+    print("Ban khong duoc giam gia!!!")
 
 
 # CAU 2
-# password = 123456
-# for i in range(1,4):
-#     input_pass = input("Nhap mat khau: ")
-#     if(input_pass == password):
-#         print("Đăng nhập thành công!")
-#         break
-#     else:
-#         if(i >= 3):
-#             print("Tài khoản đã bị khóa!")
-#             break
-#         else:
-#             print("Mật khẩu sai, vui lòng nhập lại!")
+password = 123456
+for i in range(1,4):
+    input_pass = input("Nhap mat khau: ")
+    if(input_pass == password):
+        print("Đăng nhập thành công!")
+        break
+    else:
+        if(i >= 3):
+            print("Tài khoản đã bị khóa!")
+            break
+        else:
+            print("Mật khẩu sai, vui lòng nhập lại!")
 
 
 # CAU 3
